7929c9fff9989f074faf88c012c67e0c1e6fb2ab
Costy Foglio <cfogliojr@icloud.com>
2024-12-08 22:32:15 -0500

    remove white square from liked icon, change avatar modal to fit figma design, add loading button text to avatar submit button

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,33 +12,6 @@ import { setButtonText } from "../utils/helpers.js";
 const headerLogoImg = document.getElementById("header-logo");
 headerLogoImg.src = headerLogoSrc;
 
-// const initialCards = [
-//   {
-//     name: "Val Thorens",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-//   {
-//     name: "Restaurant terrace",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-//   },
-//   {
-//     name: "An outdoor cafe",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-//   },
-//   {
-//     name: "A very long bridge, over the forest and through the trees",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-//   },
-//   {
-//     name: "Tunnel with morning light",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-//   },
-//   {
-//     name: "Mountain house",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-// ];
-
 const api = new Api({
   baseUrl: "https://around-api.en.tripleten-services.com/v1",
   headers: {
@@ -103,10 +76,8 @@ api
       cardsList.append(cardEl);
     });
 
-    profileName.textContent = userInfo.about;
-    profileDescription.textContent = userInfo.name;
-    profileAvatar.src = userInfo.avatar;
-
+    profileName.textContent = userInfo.name;
+    profileDescription.textContent = userInfo.about;
     profileAvatar.src = userInfo.avatar;
   })
   .catch(console.error);
@@ -250,13 +221,17 @@ function handleAddCardSubmit(evt) {
 
 function handleAvatarSubmit(evt) {
   evt.preventDefault();
+  setButtonText(avatarSubmitButton, true);
   api
     .editAvatarInfo(avatarInput.value)
     .then((data) => {
       profileAvatar.src = data.avatar;
       closeModal(avatarModal);
     })
-    .catch(console.error);
+    .catch(console.error)
+    .finally(() => {
+      setButtonText(avatarSubmitButton, false);
+    });
 }
 
 profileEditButton.addEventListener("click", () => {
